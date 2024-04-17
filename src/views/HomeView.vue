7407609f4d0ac2313c59d4fbfc2b3afb8ffca21e
b-regan11 <reganbill02@gmail.com>
2024-04-17 16:31:43 -0400

    Basic Home Page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="headshot_container">
+      <img class="headshot" src="../assets/Headshots/LinkedInHeadshot.jpg" alt="Bill Regan">
+    </div>
+    <div>
+      <h2>Welcome to my portfolio site</h2>
+      <h2>My name is Bill Regan</h2>
+      <h2>You can learn a little more about me in the 
+        <router-link to="/about">About</router-link> tab
+      </h2>
+      <h2>
+        You can view my past project work in the 
+        <router-link to="/projects">Projects</router-link> tab
+      </h2>
+      <h3>
+        There is more to come but for now you can see what I'm working on now 
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">here</a>
+      </h3>
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
+
+<style>
+
+</style>
